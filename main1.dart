c8ea9a97250631bba15ae9bd4a3a333ef54d4500
aymenToronto <aymenharkati110@gmail.com>
2025-11-12 13:56:33 -0500

    Ajout de la solution de la question 6

--- main1.dart
+++ main1.dart
@@ -13,3 +13,12 @@ class Couleur {
     print('(R: $r, V: $g, B: $b)');
  }
 }
+void main() {
+
+  var rouge = Couleur.rouge();
+  var vert = Couleur.vert();
+  var bleu = Couleur.bleu();
+  rouge.afficherCouleur(); // (R: 255, V: 0, B: 0)
+  vert.afficherCouleur();   // (R: 0, V: 255, B: 0)
+  bleu.afficherCouleur();   // (R: 0, V: 0, B: 255)
+}
