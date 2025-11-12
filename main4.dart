8ba77b62271e5861328308154ca6efcf8c043b8c
aymenToronto <aymenharkati110@gmail.com>
2025-11-12 16:00:39 -0500

    ajout la question 6

--- main4.dart
+++ main4.dart
@@ -6,4 +6,13 @@ class Media {
   }
 
 }
+class Livre extends Media {
+  final String auteur;
+
+  Livre(String titre, this.auteur) : super(titre);
+  @override
+  void afficherType() {
+    print("Ceci est un Livre : \"$titre\" par $auteur.");
+  }
+}
   
