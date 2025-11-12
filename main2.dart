d2377e09cdd57d6bd1b734b8ce9333659642b5b8
aymenToronto <aymenharkati110@gmail.com>
2025-11-12 14:33:28 -0500

    Ajout de la solution de la question 4

--- main2.dart
+++ main2.dart
@@ -3,5 +3,16 @@ class Point {
   double _x;
   double _y;
 
-   Point(this._x, this._y);
+  Point(this._x, this._y);
+
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
+
+
+void display() {
+  print('Point: ($_x, $_y)');
+}
+
 }
