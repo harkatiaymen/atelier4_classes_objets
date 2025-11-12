c0dd2e8358f644558560353db4144d1bd7d389d3
aymenToronto <aymenharkati110@gmail.com>
2025-11-12 16:06:23 -0500

    ajout la question 11

--- main4.dart
+++ main4.dart
@@ -24,3 +24,16 @@ class Film extends Media {
     print("Ceci est un Film : \"$titre\" (${dureeMinutes} min).");
   }
 }  
+void main() {
+  List<Media> catalogue = [
+    Livre("1984", "George Orwell"),
+    Livre("Le Petit Prince", "Antoine de Saint-Exupéry"),
+    Film("Inception", 148),
+    Film("Avatar", 162),
+  ];
+  for (var media in catalogue) {
+    media.afficherType();
+  }
+  }
+
+
