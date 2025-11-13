12f6b78fb7343421c2e394ebf601d833aef597a0
aymenToronto <aymenharkati110@gmail.com>
2025-11-13 13:51:40 -0500

    ajout la question 9

--- main8.dart
+++ main8.dart
@@ -15,3 +15,13 @@ class Livre {
       print("Nombre total de livres créés : $totalLivres");
     }
   }
+  class Roman extends Livre {
+     String genre;
+      Roman(String titre, String auteur, this.genre) : super(titre, auteur);
+
+      @override
+      void afficherInfos() {
+        print("Titre: $titre, Auteur: $auteur, Genre: $genre, Pages: $pages");
+      }
+
+  }
