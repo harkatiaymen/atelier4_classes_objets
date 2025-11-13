5790e7ecb32c8895dc9a708eb60663a4dd51be59
aymenToronto <aymenharkati110@gmail.com>
2025-11-13 13:27:28 -0500

    ajout la question 5

--- main7.dart
+++ main7.dart
@@ -10,6 +10,7 @@ void main() {
   var t1 = Tache("Faire les devoirs");
   var t2 = Tache("Aller au supermarché");
   var t3 = Tache("Faire du sport");
+   print("${t1.description}, ${t2.description}, ${t3.description}");
 
   print("Nombre total de tâches créées : ${Tache.nombreTotal}");
 }
